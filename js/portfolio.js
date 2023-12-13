128fa51bdc0715d5469ed42bf1a5ac23173d64e4
matheusguermandi <matheus_guermandi@hotmail.com>
2023-12-13 20:23:01 +0000

    (js) feat: add convert between currencies with defined exchange rates method implementation

--- js/portfolio.js
+++ js/portfolio.js
@@ -16,11 +16,17 @@ class Portfolio {
   }
 
   convert(money, currency) {
-    var eurToUsd = 1.2;
+    var exchangeRates = new Map();
+    exchangeRates.set("EUR->USD", 1.2);
+    exchangeRates.set("USD->KRW", 1100);
+
     if (money.currency == currency) {
       return money.amount;
     }
-    return money.amount * eurToUsd;
+
+    let key = money.currency + "->" + currency;
+
+    return money.amount * exchangeRates.get(key);
   }
 }
 
