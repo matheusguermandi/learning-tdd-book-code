121f25e4fc257db6147c28ba7891bf405ad0e193
matheusguermandi <matheus_guermandi@hotmail.com>
2023-12-13 20:24:15 +0000

    (py) feat: add convert between currencies with defined exchange rates method implementation

--- py/portfolio.py
+++ py/portfolio.py
@@ -12,11 +12,13 @@ class Portfolio:
 
     def evaluate(self, currency):
         total = functools.reduce(operator.add, 
-          map(lambda m: self.__convert(m, currency), self.moneys), 0) # <1>
+          map(lambda m: self.__convert(m, currency), self.moneys), 0)
         return Money(total, currency)
 
-    def __convert(self, aMoney, aCurrency):
+      def __convert(self, aMoney, aCurrency):
+        exchangeRates = {'EUR->USD': 1.2, 'USD->KRW': 1100}
         if aMoney.currency == aCurrency:
             return aMoney.amount
         else:
-            return aMoney.amount * self._eur_to_usd
+            key = aMoney.currency + '->' + aCurrency
+            return aMoney.amount * exchangeRates[key]
