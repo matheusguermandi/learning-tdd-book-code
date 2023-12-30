37188160bf075bf49632a8d291d5c0446246aaa1
matheusguermandi <matheus_guermandi@hotmail.com>
2023-12-30 17:06:48 +0000

    (go) feat: add add money method

--- go/stocks/money.go
+++ go/stocks/money.go
@@ -17,3 +17,11 @@ func (m Money) Divide(divisor int) Money {
 	return Money{amount: m.amount / float64(divisor), currency: m.currency}
 }
 
+func (m Money) Add(other *Money) *Money {
+	var result Money
+	if m.currency == other.currency {
+		result = Money{amount: m.amount + other.amount, currency: m.currency}
+		return &result
+	}
+	return nil
+}
